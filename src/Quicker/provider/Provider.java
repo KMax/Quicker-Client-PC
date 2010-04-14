f0e435c88678f6d1b7690fdc82ccb146066ad467
KMax <kolchinmax@gmail.com>
2010-04-14 23:07:58 +0400

    Deleted NotesProvider. Rewrited Provider

--- src/Quicker/provider/Provider.java
+++ src/Quicker/provider/Provider.java
@@ -1,6 +1,8 @@
 package Quicker.provider;
 
+import com.rsa.jsafe.provider.JsafeJCE;
 import com.rsa.jsafe.provider.SecureRandomEx;
+import com.rsa.jsse.JsseProvider;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.client.urlconnection.HTTPSProperties;
@@ -10,13 +12,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
@@ -31,45 +32,30 @@ import javax.net.ssl.X509TrustManager;
  */
 public class Provider {
 
-	public static String user = "maxim";
-	private static String pass = "123";
-	private static URL serverURL = null;
+	public String user;
+	private URL serverURL;
 	private Client client;
-	private KeyStore ks = null;
-	private SSLContext ssl = null;
-	private static Provider instance = null;
+	private KeyStore ks;
+	private SSLContext ssl;
+	private char[] trustPass = {'c', 'h', 'a', 'n', 'g', 'e', 'i', 't'};
+	private JsseProvider rsaJsseProvider = new JsseProvider();
+	private JsafeJCE rsaJceProvider = new JsafeJCE();
 
-	private Provider(){
-		
+	public Provider(URL host, String user, String pass) {
+		this.user = user;
+		this.serverURL = host;
+
+		SavingTrustManager tm = null;
 		try {
-			serverURL = new URL("HTTPS", "localhost", 8181, "/Quicker/");
-		} catch (MalformedURLException ex) {
-			ex.printStackTrace();
-		}
-		
-		try {
-			ks = KeyStore.getInstance(KeyStore.getDefaultType());
-			InputStream in;
-			in = new FileInputStream(getCacerts());
-			ks.load(in, "changeit".toCharArray());
-			in.close();
+			TrustManagerFactory tmf = createTrustManagerFactory();
+			X509TrustManager defaultTrustManager = (X509TrustManager) tmf.getTrustManagers()[0];
+			tm = new SavingTrustManager(defaultTrustManager);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
-		SavingTrustManager tm = null;
-		try{
-			TrustManagerFactory tmf =
-					TrustManagerFactory.getInstance(TrustManagerFactory.
-					getDefaultAlgorithm());
-			tmf.init(ks);
-			X509TrustManager defaultTrustManager = (X509TrustManager) tmf.getTrustManagers()[0];
-			tm = new SavingTrustManager(defaultTrustManager);
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
-		
+
 		HostnameVerifier hv = new HostnameVerifier() {
+
 			@Override
 			public boolean verify(String urlHostName, SSLSession session) {
 				if (urlHostName.equals(session.getPeerHost())) {
@@ -79,20 +65,20 @@ public class Provider {
 				}
 			}
 		};
-		
-		
-		try{
+
+
+		try {
 			ssl = SSLContext.getInstance("TLSv1");
 			ssl.init(null, new TrustManager[]{tm}, SecureRandomEx.getInstance("HMACDRBG"));
 			HTTPSProperties https = new HTTPSProperties(hv, ssl);
 			client = Client.create();
 			client.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, https);
 			client.addFilter(new HTTPBasicAuthFilter(user, pass));
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
-		if(!IsCertAlreadyTrusted()){
+		if (!IsCertAlreadyTrusted()) {
 			X509Certificate[] chain = tm.chain;
 			X509Certificate cert = chain[0];
 			addCertInStore(serverURL.getHost(), cert);
@@ -103,16 +89,16 @@ public class Provider {
 	 * Try out "Handshake"
 	 * @return
 	 */
-	public boolean IsCertAlreadyTrusted(){
-		try{
+	public boolean IsCertAlreadyTrusted() {
+		try {
 			SSLSocketFactory factory = ssl.getSocketFactory();
-			SSLSocket socket = (SSLSocket)factory.createSocket(
+			SSLSocket socket = (SSLSocket) factory.createSocket(
 					serverURL.getHost(), serverURL.getPort());
 			socket.setSoTimeout(10000);
 			socket.startHandshake();
 			socket.close();
-		}catch(Exception ex){
-			System.out.println(ex.getMessage());
+		} catch (Exception ex) {
+			ex.printStackTrace();
 			return false;
 		}
 		return true;
@@ -123,55 +109,68 @@ public class Provider {
 	 * @param alias - Name of host for certificate's record
 	 * @param chain - Certificate
 	 */
-	private void addCertInStore(String alias, X509Certificate chain){
-		try{
+	private void addCertInStore(String alias, X509Certificate chain) {
+		try {
 			ks.setCertificateEntry(alias, chain);
-			OutputStream out = new FileOutputStream(getCacerts());
+			OutputStream out = new FileOutputStream(getTrustStore());
 			ks.store(out, "changeit".toCharArray());
 			out.close();
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
-	public static Provider getInstance(){
-		if(instance == null){
-			instance = new Provider();
-		}
-		return instance;
-	}
-
-	public <T> T get(String uri,Class<T> t){
+	public <T> T get(String uri, Class<T> t) {
 		return client.resource(serverURL.toString() + uri).get(t);
 	}
 
-	public void delete(String uri){
-		client.resource(serverURL.toString()+uri).delete();
+	public void delete(String uri) {
+		client.resource(serverURL.toString() + uri).delete();
 	}
 
-	public void put(String uri,Object t){
-		client.resource(serverURL.toString()+uri).put(t);
+	public void put(String uri, Object t) {
+		client.resource(serverURL.toString() + uri).put(t);
 	}
 
-	public void post(String uri,Object t){
-		client.resource(serverURL.toString()+uri).post(t);
+	public void post(String uri, Object t) {
+		client.resource(serverURL.toString() + uri).post(t);
 	}
 
 	/**
 	 *
-	 * @return java.io.File - file contains trusted certificates
+	 * @return - file contains trusted certificates
 	 */
-	private File getCacerts(){
+	private File getTrustStore() {
 		File file = new File("jssecacerts");
 		if (file.isFile() == false) {
-			char SEP = File.separatorChar;
-			File dir = new File(System.getProperty("java.home") + SEP
-					+ "lib" + SEP + "security");
-			file = new File(dir, "jssecacerts");
-			if (file.isFile() == false) {
-				file = new File(dir, "cacerts");
+			try {
+				file.createNewFile();
+				//			char SEP = File.separatorChar;
+				//			File dir = new File(System.getProperty("java.home") + SEP
+				//					+ "lib" + SEP + "security");
+				//			file = new File(dir, "jssecacerts");
+				//			if (file.isFile() == false) {
+				//			}
+				//			}
+			} catch (IOException ex) {
+				ex.printStackTrace();
 			}
 		}
 		return file;
+	}
+
+	private TrustManagerFactory createTrustManagerFactory()
+			throws NoSuchAlgorithmException, KeyStoreException,
+			IOException, CertificateException {
+
+		ks = KeyStore.getInstance("PKCS12", rsaJceProvider);
+		InputStream in = new FileInputStream(getTrustStore());
+		ks.load(in, trustPass);
+		in.close();
+
+		TrustManagerFactory tmf =
+				TrustManagerFactory.getInstance(JsseProvider.PKIX, rsaJsseProvider);
+		tmf.init(ks);
+		return tmf;
 	}
 }
